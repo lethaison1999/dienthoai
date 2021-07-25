4aae81b98bf9050f4916f37f271fd02c49dd7e29
Lê Thái Sơn <lethaison04011999@gmail.com>
2021-07-25 21:20:03 +0700

    hello

---
+++ home_include/menu_ngang.php
@@ -0,0 +1,11 @@
+                <div id="menu">
+					<ul>
+						<li><a href="index.php"><i class="fas fa-home"></i>TRANG CHỦ</a></li>
+						<li><a href="index.php?content=gioithieu"><i class="far fa-clone"></i>GIỚI THIỆU</a></li>
+						<li><a href="index.php?content=sanpham"><i class="fas fa-mobile-alt"></i>SẢN PHẨM</a>
+						<li><a href="index.php?content=phukien"><i class="fas fa-headphones-alt"></i>PHỤ KIỆN</a></li>
+						<li><a href="index.php?content=khuyenmai"><i class="fas fa-percent"></i>KHUYỂN MÃI</a></li>
+						<li><a href="index.php?content=tintuc"><i class="far fa-newspaper"></i>TIN TỨC</a></li>
+						<li><a href="index.php?content=hotro"><i class="fas fa-headset"></i>HỖ TRỢ</a></li>
+					</ul>
+				</div>
